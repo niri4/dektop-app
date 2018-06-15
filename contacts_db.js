4f1ae6f0cc49da41d8fbc9e86a147ae58ed588fd
Nirankar Singh <nirankarsingh247@gmail.com>
2018-06-15 20:55:34 +0530

    add contacts

--- contacts_db.js
+++ contacts_db.js
@@ -1,5 +1,6 @@
 // this is called when an error happens in a transaction
 function errorHandler(transaction, error) {
+  alert('Error: ' + error.message + ' code: ' + error.code);
    console.log('Error: ' + error.message + ' code: ' + error.code);
 
 }
@@ -11,18 +12,18 @@ function successCallBack() {
 }
 //This is for Listing values present in the database
 function ListDBValues() {
-   $('#product_id').html('');
+   $('#contact_id').html('');
    var db = openDatabase('mydb-test', '1.0', 'sqllite test database', 2 * 1024 * 1024);
    db.transaction(function (transaction) {
-       transaction.executeSql('SELECT * FROM Product;', [],
+       transaction.executeSql('SELECT * FROM Contact;', [],
            function (transaction, result) {
                if (result != null && result.rows != null) {
                    console.log(result.rows)
                    for (var i = 0; i < result.rows.length; i++) {
                        var row = result.rows.item(i);
                        console.log(row.Name)
-                       $('#product_id').append("<tr>" + "\n" + "<td>" + row.Id + "</td>" + "\n" + "<td>" + row.product_name + "</td>" +
-                           "\n"  + "<td>" + row.product_price + "</td>" + "\n" + "\n" + "<td>" + "<button class='btn btn-primary' id='del' value=" + row.Id +  "> Delete</button>" +  "</td>" + "\n" +  "</tr>");
+                       $('#contact_id').append("<tr>" + "\n" + "<td>" + row.Id + "</td>" + "\n" + "<td>" + row.name + "</td>" +
+                           "\n"  + "<td>" + row.address + "</td>" + "\n"  + "<td>" + row.state + "</td>" + "\n"  + "<td>" + row.place + "</td>" +"\n" + "<td>" + "<button class='btn btn-primary' id='del' value=" + row.Id +  "> Delete</button>" +  "</td>" + "\n" +  "</tr>");
                    }
                }
            }, errorHandler);
@@ -33,10 +34,9 @@ function ListDBValues() {
 
 $(document).on('click', '#del', function () {
   var isd = $(this).val();
-  alert(isd);
    var db = openDatabase('mydb-test', '1.0', 'sqllite test database', 2 * 1024 * 1024);
    db.transaction(function (tx) {
-       tx.executeSql("DELETE FROM Product WHERE Id =" + isd );
+       tx.executeSql("DELETE FROM Contact WHERE Id =" + isd );
    });
    ListDBValues();
 })
@@ -47,14 +47,14 @@ $(document).ready(function () {
    var db = openDatabase('mydb-test', '1.0', 'sqllite test database', 2 * 1024 * 1024);
    db.transaction(function (tx) {
        // Create a table in if not exist
-       tx.executeSql('CREATE TABLE IF NOT EXISTS Contacts(Id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, address TEXT NOT NULL ,city TEXT NOT NULL ,state TEXT NOT NULL,country TEXT NOT NULL) ', [], nullHandler, errorHandler);
+       tx.executeSql('CREATE TABLE IF NOT EXISTS Contact(Id INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, address TEXT NOT NULL ,city TEXT NOT NULL ,state TEXT NOT NULL,address1 TEXT,country TEXT NOT NULL, place TEXT NOT NULL,pincode TEXT NOT NULL,gstin TEXT NOT NULL) ', [], nullHandler, errorHandler);
    }, errorHandler, successCallBack);
 });
 ListDBValues();
 $(document).on('click', '#submit', function () {
    var db = openDatabase('mydb-test', '1.0', 'sqllite test database', 2 * 1024 * 1024);
    db.transaction(function (tx) {
-       tx.executeSql('INSERT INTO Product (product_name, product_price) VALUES (?, ?)', [$('#ProductName').val(), $('#ProductPrice').val()], nullHandler, errorHandler);
+       tx.executeSql('INSERT INTO Contact (name, gstin,state,address,address1,place,pincode,country,city) VALUES (?,?,?,?,?,?,?,?,?)', [$('#Name').val(), $('#gstin').val(), $('#state').val(), $('#address').val(), $('#address1').val(), $('#place').val(), $('#pincode').val(), $('#country').val(),$('#city').val()], nullHandler, errorHandler);
    });
    ListDBValues();
 })
