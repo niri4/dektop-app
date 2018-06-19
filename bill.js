23aab6fa5261d49ed87ff2b58ca5debc2d712e62
Nirankar Singh <nirankarsingh247@gmail.com>
2018-06-19 20:53:23 +0530

    add details

--- bill.js
+++ bill.js
@@ -5,9 +5,86 @@ function errorHandler(transaction, error) {
    console.log('Error: ' + error.message + ' code: ' + error.code);
 
 }
+$(document.body).on('change', '.change', function()
+{
+  a = $("#selection :input").serialize();
+  var b = a.split('&');
+  //alert(b.join(',').split('='));
+  var key=[];
+  for (var i = 0; i < b.join(' ').split('=').length; i++) {
+  var ffff = b.join(' ').split('=').join(',').split(',')[i];
+
+    alert(ffff.split(' ')[1]);
+    if(i == 0) { // index is even
+      //alert(i);
+
+      //  key.push(b.join(' ').split('=').join(',').split(',')[i]);
+    }
+    else{
+      //key.push(b.join(' ').split('=').join(',').split(',')[i][-1]);
+
+    }
+}
+//alert(ar);
+    // / alert(   $("#selection :input").serialize());
+})
+function change_val(){
+   $("#selection :input").serialize();
+
+}
+$('body').on('click', '.delete_row', function() {
+   $(this).parents('tr').remove();
+   rowget();
+   change_val();
+   val = findrow();
+   if (val== 2){
+     $('.delete_row').hide();
+   }
+   else{
+     $('.delete_row').show();
+   }
+});
+
+
+function findrow() {
+    var x = document.getElementById("mytable").rows.length;
+    return x;
+}
+
+$("#insert-more").click(function () {
+    $("#mytable").each(function () {
+        var tds = '<tr>';
+        jQuery.each($('tr:last td', this), function () {
+            tds += '<td>' + $(this).html() + '</td>';
+        });
+        tds += '</tr>';
+        if ($('tbody', this).length > 0) {
+            $('tbody', this).append(tds);
+        } else {
+            $(this).append(tds);
+        }
+    });
+    rowget();
+    change_val();
+    val = findrow();
+    if (val== 2){
+      $('.delete_row').hide();
+    }
+    else{
+      $('.delete_row').show();
+    }
+});
 
 $( document ).ready(function() {
     ListDBValues();
+    ListDBProductValues();
+    val = findrow();
+    if (val== 2){
+      $('.delete_row').hide();
+    }
+    else{
+      $('.delete_row').show();
+    }
 });
 function ListDBValues() {
    $('#biller_name').html('<option>--Please Select --</option>');
@@ -22,6 +99,27 @@ function ListDBValues() {
                        console.log(row.Name)
                        $('#biller_name').append("<option value=" + row.Id + ">" + row.Id + '. ' +
                            row.name +   "</option>");
+                   }
+               }
+           }, errorHandler);
+   }, errorHandler, nullHandler);
+
+   return;
+}
+
+function ListDBProductValues() {
+   $('#product_select').html('<option>--Please Select --</option>');
+   var db = openDatabase('mydb-test', '1.0', 'sqllite test database', 2 * 1024 * 1024);
+   db.transaction(function (transaction) {
+       transaction.executeSql('SELECT * FROM Product;', [],
+           function (transaction, result) {
+               if (result != null && result.rows != null) {
+                   console.log(result.rows)
+                   for (var i = 0; i < result.rows.length; i++) {
+                       var row = result.rows.item(i);
+                       console.log(row.Name)
+                       $('#product_select').append("<option value=" + row.Id + ">" + row.Id + '. ' +
+                           row.product_name +   "</option>");
                    }
                }
            }, errorHandler);
@@ -54,6 +152,47 @@ $(document).on('change', '#biller_name', function () {
            }, errorHandler);
    }, errorHandler, nullHandler);
 })
+
+
+$(document).on('change', '#product_select', function () {
+  var isd = $(this).val();
+
+  $(this).closest('tr').find("input[id='hsn']").each(function() {
+
+    var db = openDatabase('mydb-test', '1.0', 'sqllite test database', 2 * 1024 * 1024);
+    db.transaction(function (transaction) {
+        transaction.executeSql("SELECT * FROM Product WHERE Id =" + isd + ";", [],
+            function (transaction, result) {
+                if (result != null && result.rows != null) {
+                    console.log(result.rows)
+                    for (var i = 0; i < result.rows.length; i++) {
+                        var row = result.rows.item(i);
+                        var pr = row;
+                    }
+                }
+            }, errorHandler);
+    }, errorHandler, nullHandler);
+
+
+
+  });
+})
+
+function rowget(){
+  $("#mytable td").click(function() {
+
+          var column_num = parseInt( $(this).index() ) + 1;
+           row_num = parseInt( $(this).parent().index() )+1;
+      });
+
+}
+var row_num =0;
+   $(document).ready(function(){
+     rowget();
+
+  })
+
+
 
 
 function nullHandler() {};
